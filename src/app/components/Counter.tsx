82bec706559a725fc97842dbb0e99cfa6f10ea9d
tlagoda <thibautlagoda@hotmail.fr>
2023-06-07 23:26:28 +0200

    prop + conditional rendering + list rendering

--- src/app/components/Counter.tsx
+++ src/app/components/Counter.tsx
@@ -1,22 +1,27 @@
-'use client'
+"use client";
 
-import { useState } from "react"
+import { useState } from "react";
 
-const Counter = () => {
-    const [count, setCount] = useState(0)
-    const increment = () => {
-        setCount(count + 1)
-    }
-    const decrement = () => {
-        setCount(count - 1)
-    }
-    return (
-        <>
-            <p>Hello from Counter. Count = { count }</p>
-            <button onClick={increment}>Increment</button>
-            <button onClick={decrement}>Decrement</button>
-        </>
-    )
-}
+const Counter = ({ startingCount }) => {
+  const [count, setCount] = useState(startingCount);
+  const increment = () => {
+    setCount(count + 1);
+  };
+  const decrement = () => {
+    setCount(count - 1);
+  };
 
-export default Counter
+  const scientists = ["Isaac Newton", "Albert Einstein", "Alan Turing"];
+
+  return (
+    <>
+      <p>Hello from Counter. Count = {count}</p>
+      <p>Count is {count % 2 === 0 ? "even" : "odd"}.</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      {scientists.map(scientist => <h2 key={scientist}>{scientist}</h2>)}
+    </>
+  );
+};
+
+export default Counter;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import styles from './page.module.css'
 import Counter from './components/Counter'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main>
       <h1>MyApp</h1>
-      <Counter />
+      <Counter startingCount={-1}/>
     </main>
   )
 }
